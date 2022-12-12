c1ad9bda8bedff54a4cde9f66e2aea442762ad84
Viljar Kärgenberg <viljar@remato.com>
2022-12-12 23:08:17 +0200

    asyncstorage update

--- src/utils/useAsyncStorage.ts
+++ src/utils/useAsyncStorage.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default <T>(key: string, defaultValue?: T): [T, (data: T) => T, () => void] => {
