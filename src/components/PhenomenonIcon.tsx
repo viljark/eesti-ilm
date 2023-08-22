0c5a5ab8648aa78ab6824a24730ddc3ac51341c4
Viljar Kärgenberg <viljar@remato.com>
2023-08-23 00:49:13 +0300

    add new icon theme

--- src/components/PhenomenonIcon.tsx
+++ src/components/PhenomenonIcon.tsx
@@ -14,7 +14,7 @@ import Fog from '../icons/Fog'
 import Thunder from '../icons/Thunder'
 import ThunderStorm from '../icons/ThunderStorm'
 import Hail from '../icons/Hail'
-import { Dimensions, StyleProp, StyleSheet, StyleSheetProperties, ViewStyle } from 'react-native'
+import { Dimensions, StyleProp, ViewStyle, Image, View } from 'react-native'
 import _ from 'lodash'
 import SnowStorm from '../icons/SnowStorm'
 import LottieView from 'lottie-react-native'
@@ -32,8 +32,60 @@ import snowstormLottie from '@bybas/weather-icons/production/fill/lottie/wind-sn
 import sleetLottie from '@bybas/weather-icons/production/fill/lottie/sleet.json'
 import glazeLottie from '@bybas/weather-icons/production/fill/lottie/snowflake.json'
 import fogLottie from '@bybas/weather-icons/production/fill/lottie/fog.json'
-import thunderLottie from '@bybas/weather-icons/production/fill/lottie/thunderstorms-overcast-rain.json'
+import thunderLottie from '@bybas/weather-icons/production/fill/lottie/thunderstorms-overcast.json'
 import thunderStormLottie from '@bybas/weather-icons/production/fill/lottie/thunderstorms-extreme-rain.json'
+import lightShowerDayLottie from '@bybas/weather-icons/production/fill/lottie/partly-cloudy-day-rain.json'
+import lightShowerNightLottie from '@bybas/weather-icons/production/fill/lottie/partly-cloudy-night-rain.json'
+import moderateShowerDayLottie from '@bybas/weather-icons/production/fill/lottie/overcast-day-rain.json'
+import moderateShowerNightLottie from '@bybas/weather-icons/production/fill/lottie/overcast-night-rain.json'
+import strongShowerDayLottie from '@bybas/weather-icons/production/fill/lottie/extreme-day-rain.json'
+import strongShowerNightLottie from '@bybas/weather-icons/production/fill/lottie/extreme-night-rain.json'
+
+// @ts-ignore
+import overcastMeteocon from '@bybas/weather-icons/production/fill/png/256/overcast.png'
+// @ts-ignore
+import hailMeteocon from '@bybas/weather-icons/production/fill/png/256/extreme-hail.png'
+// @ts-ignore
+import clearDayMeteocon from '@bybas/weather-icons/production/fill/png/256/clear-day.png'
+// @ts-ignore
+import clearNightMeteocon from '@bybas/weather-icons/production/fill/png/256/clear-night.png'
+// @ts-ignore
+import fewCloudsDayMeteocon from '@bybas/weather-icons/production/fill/png/256/partly-cloudy-day.png'
+// @ts-ignore
+import fewCloudsNightMeteocon from '@bybas/weather-icons/production/fill/png/256/partly-cloudy-night.png'
+// @ts-ignore
+import lightRainMeteocon from '@bybas/weather-icons/production/fill/png/256/rain.png'
+// @ts-ignore
+import moderateRainMeteocon from '@bybas/weather-icons/production/fill/png/256/overcast-rain.png'
+// @ts-ignore
+import strongRainMeteocon from '@bybas/weather-icons/production/fill/png/256/extreme-rain.png'
+// @ts-ignore
+import snowMeteocon from '@bybas/weather-icons/production/fill/png/256/overcast-snow.png'
+// @ts-ignore
+import snowstormMeteocon from '@bybas/weather-icons/production/fill/png/256/wind-snow.png'
+// @ts-ignore
+import sleetMeteocon from '@bybas/weather-icons/production/fill/png/256/sleet.png'
+// @ts-ignore
+import glazeMeteocon from '@bybas/weather-icons/production/fill/png/256/snowflake.png'
+// @ts-ignore
+import fogMeteocon from '@bybas/weather-icons/production/fill/png/256/fog.png'
+// @ts-ignore
+import thunderMeteocon from '@bybas/weather-icons/production/fill/png/256/thunderstorms-overcast.png'
+// @ts-ignore
+import thunderStormMeteocon from '@bybas/weather-icons/production/fill/png/256/thunderstorms-extreme-rain.png'
+// @ts-ignore
+import lightShowerDayMeteocon from '@bybas/weather-icons/production/fill/png/256/partly-cloudy-day-rain.png'
+// @ts-ignore
+import lightShowerNightMeteocon from '@bybas/weather-icons/production/fill/png/256/partly-cloudy-night-rain.png'
+// @ts-ignore
+import moderateShowerDayMeteocon from '@bybas/weather-icons/production/fill/png/256/overcast-day-rain.png'
+// @ts-ignore
+import moderateShowerNightMeteocon from '@bybas/weather-icons/production/fill/png/256/overcast-night-rain.png'
+// @ts-ignore
+import strongShowerDayMeteocon from '@bybas/weather-icons/production/fill/png/256/extreme-day-rain.png'
+// @ts-ignore
+import strongShowerNightMeteocon from '@bybas/weather-icons/production/fill/png/256/extreme-night-rain.png'
+import { useAssets } from 'expo-asset'
 
 // mapping https://www.ilmateenistus.ee/teenused/ilmainfo/eesti-vaatlusandmed-xml/
 const clear = ['Clear']
@@ -42,9 +94,12 @@ const overcast = ['Overcast', 'Cloudy']
 
 const snow = ['Light snow shower', 'Moderate snow shower', 'Heavy snow shower', 'Light snowfall', 'Moderate snowfall', 'Heavy snowfall', 'Blowing snow', 'Drifting snow']
 const snowStorm = ['Snowstorm']
-const lightRain = ['Light shower', 'Light rain']
-const moderateRain = ['Moderate shower', 'Moderate rain']
-const strongRain = ['Heavy shower', 'Heavy rain']
+const lightRain = ['Light rain']
+const lightShower = ['Light shower']
+const moderateRain = ['Moderate rain']
+const moderateShower = ['Moderate shower']
+const strongRain = ['Heavy rain']
+const strongShower = ['Heavy shower']
 const sleet = ['Light sleet', 'Moderate sleet']
 const glaze = ['Glaze', 'Risk of glaze']
 const fog = ['Mist', 'Fog']
@@ -76,10 +131,11 @@ interface PhenomenonIconProps {
   longitude?: number
   width?: number
   height?: number
-  style?: StyleProp<ViewStyle>
+  style?: StyleProp<any>
   children?: React.ReactNode
   isDay?: boolean
   animated?: boolean
+  theme?: 'meteocon' | 'default'
 }
 
 const height = Dimensions.get('window').height - 121 //full height
@@ -124,6 +180,9 @@ const PhenomenonIcon_: FunctionComponent<PhenomenonIconProps> = (props: Phenomen
     if (lightRain.includes(props.phenomenon)) return lightRainLottie
     if (moderateRain.includes(props.phenomenon)) return moderateRainLottie
     if (strongRain.includes(props.phenomenon)) return strongRainLottie
+    if (lightShower.includes(props.phenomenon)) return isDay ? lightShowerDayLottie : lightShowerNightLottie
+    if (moderateShower.includes(props.phenomenon)) return isDay ? moderateShowerDayLottie : moderateShowerNightLottie
+    if (strongShower.includes(props.phenomenon)) return isDay ? strongShowerDayLottie : strongShowerNightLottie
     if (sleet.includes(props.phenomenon)) return sleetLottie
     if (glaze.includes(props.phenomenon)) return glazeLottie
     if (fog.includes(props.phenomenon)) return fogLottie
@@ -132,8 +191,53 @@ const PhenomenonIcon_: FunctionComponent<PhenomenonIconProps> = (props: Phenomen
     if (hail.includes(props.phenomenon)) return hailLottie
   }, [isDay, props.phenomenon])
 
+  const meteoconIcon = useMemo(() => {
+    if (clear.includes(props.phenomenon)) return isDay ? clearDayMeteocon : clearNightMeteocon
+    if (fewClouds.includes(props.phenomenon)) return isDay ? fewCloudsDayMeteocon : fewCloudsNightMeteocon
+    if (overcast.includes(props.phenomenon)) return overcastMeteocon
+    if (snow.includes(props.phenomenon)) return snowMeteocon
+    if (snowStorm.includes(props.phenomenon)) return snowstormMeteocon
+    if (lightRain.includes(props.phenomenon)) return lightRainMeteocon
+    if (moderateRain.includes(props.phenomenon)) return moderateRainMeteocon
+    if (strongRain.includes(props.phenomenon)) return strongRainMeteocon
+    if (lightShower.includes(props.phenomenon)) return isDay ? lightShowerDayMeteocon : lightShowerNightMeteocon
+    if (moderateShower.includes(props.phenomenon)) return isDay ? moderateShowerDayMeteocon : moderateShowerNightMeteocon
+    if (strongShower.includes(props.phenomenon)) return isDay ? strongShowerDayMeteocon : strongShowerNightMeteocon
+    if (sleet.includes(props.phenomenon)) return sleetMeteocon
+    if (glaze.includes(props.phenomenon)) return glazeMeteocon
+    if (fog.includes(props.phenomenon)) return fogMeteocon
+    if (thunder.includes(props.phenomenon)) return thunderMeteocon
+    if (thunderStorm.includes(props.phenomenon)) return thunderStormMeteocon
+    if (hail.includes(props.phenomenon)) return hailMeteocon
+  }, [isDay, props.phenomenon])
+
+  const [assets] = useAssets([meteoconIcon || clearDayMeteocon])
   if (props.animated) {
     return <LottieIcon style={lottieStyle} path={lottiePath} />
+  }
+
+  const w = props.width || size
+  const h = props.height || size
+  const wOffset = w / 2
+  const hOffset = h / 2
+  if (props.theme === 'meteocon') {
+    if (!assets?.[0]?.localUri) {
+      return null
+    }
+    return (
+      <View style={[props.style, { width: w, height: h }]}>
+        <Image
+          source={{ uri: assets?.[0]?.localUri }}
+          width={w + wOffset}
+          height={h + hOffset}
+          style={{
+            position: 'absolute',
+            left: -wOffset / 2,
+            top: -hOffset / 2,
+          }}
+        />
+      </View>
+    )
   }
 
   return (
@@ -146,6 +250,9 @@ const PhenomenonIcon_: FunctionComponent<PhenomenonIconProps> = (props: Phenomen
       {lightRain.includes(props.phenomenon) && <LightRain {...iconProps} />}
       {moderateRain.includes(props.phenomenon) && <StrongRain {...iconProps} />}
       {strongRain.includes(props.phenomenon) && <StrongRain {...iconProps} />}
+      {lightShower.includes(props.phenomenon) && <LightRain {...iconProps} />}
+      {moderateShower.includes(props.phenomenon) && <StrongRain {...iconProps} />}
+      {strongShower.includes(props.phenomenon) && <StrongRain {...iconProps} />}
       {sleet.includes(props.phenomenon) && <Sleet {...iconProps} />}
       {glaze.includes(props.phenomenon) && <Glaze {...iconProps} />}
       {fog.includes(props.phenomenon) && <Fog {...iconProps} />}
@@ -164,5 +271,5 @@ PhenomenonIcon_.defaultProps = {
 
 const LottieIcon = memo(LottieIcon_)
 function LottieIcon_({ style, path }: { path: any; style: StyleProp<ViewStyle> }) {
-  return <LottieView autoPlay style={style} source={path} hardwareAccelerationAndroid={true} />
+  return <LottieView autoPlay style={style} source={path} />
 }
